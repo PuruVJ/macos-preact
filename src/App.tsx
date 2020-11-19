7f023666021b5a62b5c24236ba36a7e5e08cb5f0
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2020-11-19 12:58:30 +0530

    Remove eslint, install restater (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,27 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-interface AppProps {}
-
-function App({}: AppProps) {
+function App() {
   // Create the count state.
   const [count, setCount] = useState(1);
   // Create the counter (+1 every second).
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count => count + 1), 1000);
+    const timer = setTimeout(() => setCount((c) => c + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
   // Return the App component.
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>src/App.tsx</code>
+          and save to reload.
         </p>
         <p>
-          Page has been open for <code>{count}</code> seconds.
+          Page has been open for .<code>{count}</code>
+          seconds.
         </p>
         <p>
           <a
