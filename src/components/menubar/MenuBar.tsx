0f39ae743757775681e0900bd63b1e9a83e421ed
Puru Vijay <awesomepuruvj@gmail.com>
2020-11-29 04:30:14 +0530

    Badic menu

--- src/components/menubar/MenuBar.tsx
+++ src/components/menubar/MenuBar.tsx
@@ -1,7 +1,10 @@
-import { ButtonBase, fade, makeStyles, Typography } from '@material-ui/core';
+import { ButtonBase, fade, makeStyles } from '@material-ui/core';
+import { mdiApple, mdiAppleAirplay, mdiWifiStrength4 } from '@mdi/js';
+import { format } from 'date-fns';
 import React from 'react';
+import { SwitchSVG } from '__/assets/sf-icons/switch.svg';
+import { VolumeLowSVG } from '__/assets/sf-icons/volume-low.svg';
 import { AppIcon } from '../utils/app-icons';
-import { mdiApple } from '@mdi/js';
 
 const MenuBar = (): React.ReactElement => {
   const classes = useStyles();
@@ -12,7 +15,7 @@ const MenuBar = (): React.ReactElement => {
         <ButtonBase className={classes.appleButton}>
           <AppIcon size={18} path={mdiApple} />
         </ButtonBase>
-        <ButtonBase style={{fontWeight: 600, margin: `0 6px`}}>Finder</ButtonBase>
+        <ButtonBase style={{ fontWeight: 600, margin: `0 6px` }}>Finder</ButtonBase>
 
         {/* menu buttons */}
         <ButtonBase className={classes.menuButton}>File</ButtonBase>
@@ -21,6 +24,28 @@ const MenuBar = (): React.ReactElement => {
         <ButtonBase className={classes.menuButton}>Go</ButtonBase>
         <ButtonBase className={classes.menuButton}>Window</ButtonBase>
         <ButtonBase className={classes.menuButton}>Help</ButtonBase>
+
+        <span className={classes.flex}></span>
+
+        <ButtonBase className={classes.iconButtons}>
+          <AppIcon size={24} path={mdiAppleAirplay} />
+        </ButtonBase>
+
+        <ButtonBase className={classes.iconButtons}>
+          <AppIcon size={24} path={mdiWifiStrength4} />
+        </ButtonBase>
+
+        <ButtonBase className={classes.iconButtons}>
+          <VolumeLowSVG />
+        </ButtonBase>
+
+        <ButtonBase className={classes.iconButtons}>
+          <SwitchSVG />
+        </ButtonBase>
+
+        <ButtonBase>
+          {format(new Date(), 'EEE MMM d  h:mm aa')}
+        </ButtonBase>
       </header>
     </>
   );
@@ -32,7 +57,7 @@ const useStyles = makeStyles(({ spacing, palette, typography }) => ({
     alignItems: 'center',
 
     width: '100%',
-    height: spacing(1.3),
+    height: spacing(1.4),
 
     backgroundColor: fade(palette.background.default, 0.3),
     backdropFilter: 'blur(6px)',
@@ -41,9 +66,11 @@ const useStyles = makeStyles(({ spacing, palette, typography }) => ({
     fill: palette.text.primary,
 
     '& button': {
-      fontWeight: '400',
-      fontSize: typography.subtitle2.fontSize,
+      fontWeight: '500',
+      fontSize: spacing(0.8),
       fontFamily: typography.fontFamily,
+
+      position: 'relative',
 
       padding: spacing(0, 0.2),
       margin: spacing(0, 0.3),
@@ -58,12 +85,28 @@ const useStyles = makeStyles(({ spacing, palette, typography }) => ({
     borderRadius: '30px',
 
     padding: spacing(0, 0.5),
-    margin: spacing(0, 0.5),
+    margin: spacing(0, 0.6),
   },
 
   flex: {
     flex: '1 1 auto',
   },
+
+  iconButtons: {
+    maxHeight: '100%',
+
+    margin: `${spacing(0, 0.5)} !important`,
+
+    '& svg, & svg path': {
+      height: spacing(1),
+      width: 'auto',
+
+      fill: `${palette.text.primary} !important`,
+
+      position: 'relative',
+    },
+  },
 }));
 
 export { MenuBar };
+
