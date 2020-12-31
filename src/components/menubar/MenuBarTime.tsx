c4dfa5b23073243b956ce036d0b884bb494cf45f
Puru Vijay <devpuruvj@gmail.com>
2020-12-31 17:52:08 +0530

    Refactor menu bar time comp by  using use-interval

--- src/components/menubar/MenuBarTime.tsx
+++ src/components/menubar/MenuBarTime.tsx
@@ -1,14 +1,11 @@
 import { format } from 'date-fns';
 import React from 'react';
+import { useInterval } from 'use-interval';
 
 const MenuBarTime = () => {
   const [time, setTime] = React.useState(new Date());
 
-  React.useEffect(() => {
-    const timer = setInterval(() => setTime(new Date()), 5);
-
-    return () => clearInterval(timer);
-  });
+  useInterval(() => setTime(new Date()), 2000);
 
   return <>{format(time, 'EEE MMM d  h:mm aa')}</>;
 };
