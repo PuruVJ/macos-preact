4cbd3585cf9263c9410f25686248ca1b20e375ac
Puru Vijay <devpuruvj@gmail.com>
2021-01-02 23:07:54 +0530

    use-click-outside for menubars

---
+++ src/hooks/use-click-outside.ts
@@ -0,0 +1,19 @@
+import { RefObject, useEffect } from 'react';
+
+function useOutsideClick<T extends HTMLElement>(ref: RefObject<T>, callback: () => void) {
+  const handleClick = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
+}
+
+export { useOutsideClick };
