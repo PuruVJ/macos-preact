661f7433dea6ac0c44ded5663633e3f394fdcc7e
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2021-01-02 14:18:01 +0530

    Get rid of material UI. Intellisense should be faster now (#30)
    * Get rid of material UI. Intellisense should be faster now
    
    * Styled comps breaking. Fix by installing react-is

--- src/global-provider.tsx
+++ src/global-provider.tsx
@@ -1,12 +1,3 @@
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import React, { ReactElement } from 'react';
 
-export const GlobalProvider = ({ children }: { children: ReactElement[] }) => (
-  <ThemeProvider
-    theme={createMuiTheme({
-      props: { MuiButtonBase: { disableTouchRipple: true, disableRipple: true } },
-    })}
-  >
-    {children}
-  </ThemeProvider>
-);
+export const GlobalProvider = ({ children }: { children: ReactElement[] }) => <>{children}</>;
