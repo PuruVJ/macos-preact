aa01323fb727578b7b354e50294e75a7e5557a68
Puru Vijay <devpuruvj@gmail.com>
2021-02-17 23:06:27 +0530

    Move assets, no import needed for assets

--- src/stores/dock.store.ts
+++ src/stores/dock.store.ts
@@ -1,17 +1,5 @@
 import { atom } from 'jotai';
 import type React from 'react';
-import IconCalendar from '__/assets/app-icons/calendar/256.png';
-import IconFacetime from '__/assets/app-icons/facetime/256.png';
-import IconFinder from '__/assets/app-icons/finder/256.png';
-import IconGithub from '__/assets/app-icons/github/256.png';
-import IconLaunchpad from '__/assets/app-icons/launchpad/256.png';
-import IconMail from '__/assets/app-icons/mail/256.png';
-import IconMaps from '__/assets/app-icons/maps/256.png';
-import IconMessages from '__/assets/app-icons/messages/256.png';
-import IconPhotos from '__/assets/app-icons/photos/256.png';
-import IconSafari from '__/assets/app-icons/safari/256.png';
-import IconSystemPreferences from '__/assets/app-icons/system-preferences/256.png';
-import IconPurusTwitter from '__/assets/app-icons/purus-twitter/256.png';
 
 export interface IDockItem {
   appName: string;
@@ -21,63 +9,65 @@ export interface IDockItem {
   breakBefore?: boolean;
 }
 
+const pathById = (id: string) => `/assets/app-icons/${id}/256.png`;
+
 const createDockConfig = <T>(et: { dockItems: { [key in keyof T]: IDockItem } }) => et;
 
 const initialState = createDockConfig({
   dockItems: {
     finder: {
       appName: 'Finder',
-      icon: IconFinder,
+      icon: pathById('finder'),
       isOpen: true,
     },
     launchpad: {
       appName: 'Launchpad',
-      icon: IconLaunchpad,
+      icon: pathById('launchpad'),
       isOpen: false,
     },
     safari: {
       appName: 'Safari',
-      icon: IconSafari,
+      icon: pathById('safari'),
       isOpen: false,
     },
     messages: {
       appName: 'Messages',
-      icon: IconMessages,
+      icon: pathById('messages'),
       isOpen: false,
     },
     mail: {
       appName: 'Mail',
-      icon: IconMail,
+      icon: pathById('mail'),
       isOpen: false,
     },
     maps: {
       appName: 'Maps',
-      icon: IconMaps,
+      icon: pathById('maps'),
       isOpen: false,
     },
     photos: {
       appName: 'Photos',
-      icon: IconPhotos,
+      icon: pathById('photos'),
       isOpen: false,
     },
     facetime: {
       appName: 'Facetime',
-      icon: IconFacetime,
+      icon: pathById('facetime'),
       isOpen: false,
     },
     calendar: {
       appName: 'Calendar',
-      icon: IconCalendar,
+      icon: pathById('calendar'),
       isOpen: false,
     },
     systemPreferences: {
       appName: 'System Preferences',
-      icon: IconSystemPreferences,
+      icon: pathById('system-preferences'),
       isOpen: false,
     },
     purusTwitter: {
       appName: `My Twitter`,
-      icon: IconPurusTwitter,
+      icon: pathById('purus-twitter'),
       isOpen: false,
       breakBefore: true,
       action: () => {
@@ -87,7 +77,7 @@ const initialState = createDockConfig({
 
     viewSource: {
       appName: 'View Source',
-      icon: IconGithub,
+      icon: pathById('github'),
       isOpen: false,
       action: () => {
         window.open('https://github.com/puruvj/macos-web', '_blank');
