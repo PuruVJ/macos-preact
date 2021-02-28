0a65935ba8cf7384c88d0c6449474728f0a81c07
Puru Vijay <devpuruvj@gmail.com>
2021-02-28 13:16:59 +0530

    🚢 Merge imports

--- src/components/menubar/ActionCenter/ActionCenter.tsx
+++ src/components/menubar/ActionCenter/ActionCenter.tsx
@@ -1,7 +1,6 @@
 import { mdiBluetooth, mdiKeyboard, mdiWifiStrength4 } from '@mdi/js';
 import { FC } from 'react';
-import styled from 'styled-components';
-import { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 import { AirDropSVG } from '__/assets/sf-icons/AirDrop.svg';
 import { MoonSVG } from '__/assets/sf-icons/Moon.svg';
 import { AppIcon } from '__/components/utils/AppIcon';
