1c88682acef89895845d0eb64cddc5278609e316
Puru Vijay <devpuruvj@gmail.com>
2021-03-24 15:19:19 +0530

    Attention message

--- src/components/topbar/ActionCenter/ActionCenter.tsx
+++ src/components/topbar/ActionCenter/ActionCenter.tsx
@@ -1,3 +1,29 @@
+/**
+ * 
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
+               AAA         TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNTTTTTTTTTTTTTTTTTTTTTTTIIIIIIIIII     OOOOOOOOO     NNNNNNNN        NNNNNNNN
+              A:::A        T:::::::::::::::::::::TT:::::::::::::::::::::TE::::::::::::::::::::EN:::::::N       N::::::NT:::::::::::::::::::::TI::::::::I   OO:::::::::OO   N:::::::N       N::::::N
+             A:::::A       T:::::::::::::::::::::TT:::::::::::::::::::::TE::::::::::::::::::::EN::::::::N      N::::::NT:::::::::::::::::::::TI::::::::I OO:::::::::::::OO N::::::::N      N::::::N
+            A:::::::A      T:::::TT:::::::TT:::::TT:::::TT:::::::TT:::::TEE::::::EEEEEEEEE::::EN:::::::::N     N::::::NT:::::TT:::::::TT:::::TII::::::IIO:::::::OOO:::::::ON:::::::::N     N::::::N
+           A:::::::::A     TTTTTT  T:::::T  TTTTTTTTTTTT  T:::::T  TTTTTT  E:::::E       EEEEEEN::::::::::N    N::::::NTTTTTT  T:::::T  TTTTTT  I::::I  O::::::O   O::::::ON::::::::::N    N::::::N
+          A:::::A:::::A            T:::::T                T:::::T          E:::::E             N:::::::::::N   N::::::N        T:::::T          I::::I  O:::::O     O:::::ON:::::::::::N   N::::::N
+         A:::::A A:::::A           T:::::T                T:::::T          E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N        T:::::T          I::::I  O:::::O     O:::::ON:::::::N::::N  N::::::N
+        A:::::A   A:::::A          T:::::T                T:::::T          E:::::::::::::::E   N::::::N N::::N N::::::N        T:::::T          I::::I  O:::::O     O:::::ON::::::N N::::N N::::::N
+       A:::::A     A:::::A         T:::::T                T:::::T          E:::::::::::::::E   N::::::N  N::::N:::::::N        T:::::T          I::::I  O:::::O     O:::::ON::::::N  N::::N:::::::N
+      A:::::AAAAAAAAA:::::A        T:::::T                T:::::T          E::::::EEEEEEEEEE   N::::::N   N:::::::::::N        T:::::T          I::::I  O:::::O     O:::::ON::::::N   N:::::::::::N
+     A:::::::::::::::::::::A       T:::::T                T:::::T          E:::::E             N::::::N    N::::::::::N        T:::::T          I::::I  O:::::O     O:::::ON::::::N    N::::::::::N
+    A:::::AAAAAAAAAAAAA:::::A      T:::::T                T:::::T          E:::::E       EEEEEEN::::::N     N:::::::::N        T:::::T          I::::I  O::::::O   O::::::ON::::::N     N:::::::::N
+   A:::::A             A:::::A   TT:::::::TT            TT:::::::TT      EE::::::EEEEEEEE:::::EN::::::N      N::::::::N      TT:::::::TT      II::::::IIO:::::::OOO:::::::ON::::::N      N::::::::N
+  A:::::A               A:::::A  T:::::::::T            T:::::::::T      E::::::::::::::::::::EN::::::N       N:::::::N      T:::::::::T      I::::::::I OO:::::::::::::OO N::::::N       N:::::::N
+ A:::::A                 A:::::A T:::::::::T            T:::::::::T      E::::::::::::::::::::EN::::::N        N::::::N      T:::::::::T      I::::::::I   OO:::::::::OO   N::::::N        N::::::N
+AAAAAAA                   AAAAAAATTTTTTTTTTT            TTTTTTTTTTT      EEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNN      TTTTTTTTTTT      IIIIIIIIII     OOOOOOOOO     NNNNNNNN         NNNNNNN
+                                                                                                                                                                                                   
+You might have come to this file to make the brightness slider functional. Pls do not attempt that, as webkit related bugs
+ruin the whole thing, and I'd hate to reject your Pull Request. 
+Thank you for your attention
+*/
+
 import { mdiBluetooth, mdiKeyboard, mdiWifiStrength4 } from '@mdi/js';
 import styled, { css } from 'styled-components';
 import { AirDropSVG } from '__/assets/sf-icons/AirDrop.svg';
