5ff734798813e1c612effbf18bd09cc83cc7d1d8
Puru Vijay <devpuruvj@gmail.com>
2021-03-28 02:39:02 +0530

    h

--- src/components/dock/DockItem.tsx
+++ src/components/dock/DockItem.tsx
@@ -131,8 +131,6 @@ const useDockHoverAnimation = (mouseX: MotionValue<number>, ref: RefObject<HTMLI
     if (el && mouseXVal !== null) {
       const rect = el.getBoundingClientRect();
 
-      // get the x coordinate of the img DOMElement's center
-      // the left x coordinate plus the half of the width
       const imgCenterX = rect.left + rect.width / 2;
 
       // difference between the x coordinate value of the mouse pointer
