0a534f4d7b4a9b3f9818e109a1c16f4b374b3d6e
Puru Vijay <devpuruvj@gmail.com>
2021-04-16 02:40:19 +0530

    Move function down in Window.tsx

--- src/components/Desktop/Window/Window.tsx
+++ src/components/Desktop/Window/Window.tsx
@@ -28,23 +28,6 @@ class WindowRnd extends Rnd {
   base?: HTMLDivElement;
 }
 
-/**
- * Extract the x and y from the transform style of the base element using Regex
- * Why using this hacking method:
- * react-rnd uses transform and translate to shift window around instead of top
- * and left and it does not provide the access to x and y values from ref
- * @param transformStyle The transform style string. e.g. translate(1123.75px, 7px)
- * @returns The window position. e.g. { x: 1123.75, y: 7 }
- */
-function extractPositionFromTransformStyle(transformStyle: string): WindowPosition {
-  const matched = transformStyle.matchAll(/[0-9.]+/g);
-  try {
-    return { x: Number(matched.next().value[0]), y: Number(matched.next().value[0]) };
-  } catch {
-    return { x: 0, y: 0 };
-  }
-}
-
 export const Window = ({ appID }: WindowProps) => {
   const [activeAppZIndex] = useAtom(activeAppZIndexStore);
   const [activeApp, setActiveApp] = useAtom(activeAppStore);
@@ -166,3 +149,20 @@ export const Window = ({ appID }: WindowProps) => {
     </Rnd>
   );
 };
+
+/**
+ * Extract the x and y from the transform style of the base element using Regex
+ * Why using this hacking method:
+ * react-rnd uses transform and translate to shift window around instead of top
+ * and left and it does not provide the access to x and y values from ref
+ * @param transformStyle The transform style string. e.g. translate(1123.75px, 7px)
+ * @returns The window position. e.g. { x: 1123.75, y: 7 }
+ */
+function extractPositionFromTransformStyle(transformStyle: string): WindowPosition {
+  const matched = transformStyle.matchAll(/[0-9.]+/g);
+  try {
+    return { x: Number(matched.next().value[0]), y: Number(matched.next().value[0]) };
+  } catch {
+    return { x: 0, y: 0 };
+  }
+}
