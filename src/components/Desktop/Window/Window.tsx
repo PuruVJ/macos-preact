cfee794b7232a7c888a6edfe755927f260acbb98
ZhiyueYi <lane0223@hotmail.com>
2021-04-16 01:17:55 +0800

    fix: transition styles should not affect dragging and resizing

--- src/components/Desktop/Window/Window.tsx
+++ src/components/Desktop/Window/Window.tsx
@@ -23,6 +23,27 @@ type WindowPosition = {
   y: number;
 };
 
+class WindowRnd extends Rnd {
+  base?: HTMLDivElement;
+}
+
+/**
+ * Extract the x and y from the transform style of the base element using Regex
+ * Why using this hacking method:
+ * react-rnd uses transform and translate to shift window around instead of top
+ * and left and it does not provide the access to x and y values from ref
+ * @param transformStyle The transform style string. e.g. translate(1123.75px, 7px)
+ * @returns The window position. e.g. { x: 1123.75, y: 7 }
+ */
+function extractPositionFromTransformStyle(transformStyle: string): WindowPosition {
+  const matched = transformStyle.matchAll(/[0-9.]+/g);
+  try {
+    return { x: Number(matched.next().value[0]), y: Number(matched.next().value[0]) };
+  } catch {
+    return { x: 0, y: 0 };
+  }
+}
+
 export const Window = ({ appID }: WindowProps) => {
   const [activeAppZIndex] = useAtom(activeAppZIndexStore);
   const [activeApp, setActiveApp] = useAtom(activeAppStore);
@@ -34,16 +55,13 @@ export const Window = ({ appID }: WindowProps) => {
   const randX = useMemo(() => randint(-600, 600), []);
   const randY = useMemo(() => randint(-100, 100), []);
 
-  const [originalSize, setOriginalSize] = useState<WindowSize>({ height: 0, width: 0 });
-  const [originalPosition, setOriginalPosition] = useState<WindowPosition>({
+  const windowRef = useRef<WindowRnd>();
+  const originalSizeRef = useRef<WindowSize>({ height: 0, width: 0 });
+  const originalPositionRef = useRef<WindowPosition>({
     x: 0,
     y: 0,
   });
-  const [windowSize, setWindowSize] = useState<WindowSize>({ height: 500, width: 600 });
-  const [windowPosition, setWindowPosition] = useState<WindowPosition>({
-    x: ((3 / 2) * document.body.clientWidth + randX) / 2,
-    y: (100 + randY) / 2,
-  });
+  const transitionClearanceRef = useRef<number>();
 
   const { resizable } = appsConfig[appID];
 
@@ -56,26 +74,57 @@ export const Window = ({ appID }: WindowProps) => {
   }, []);
 
   const maximizeApp = () => {
+    if (!windowRef?.current?.resizableElement?.current || !windowRef?.current?.base) {
+      return;
+    }
+
+    // Get desktop height and width
     const dockElementHeight = document.getElementById('dock')?.clientHeight ?? 0;
     const topBarElementHeight = document.getElementById('top-bar')?.clientHeight ?? 0;
-
     const desktopHeight = document.body.clientHeight - dockElementHeight - topBarElementHeight;
     const deskTopWidth = document.body.clientWidth;
 
+    // Get current height and width
+    const {
+      clientWidth: windowWidth,
+      clientHeight: windowHeight,
+    } = windowRef.current.resizableElement.current;
+
+    // Get current left and top position
+    const { x: windowLeft, y: windowTop } = extractPositionFromTransformStyle(
+      windowRef.current.base.style.transform,
+    );
+
+    // Only when maximizing (not dragging or resizing), should it have transaction
+    windowRef.current.base.style.transition = 'height 0.5s, width 0.5s, transform 0.5s';
+
+    // Prevent removing transition styles when multiple times of maximizing action takes place in a short period
+    clearTimeout(transitionClearanceRef.current);
+
+    // Transition style gets cleared after 0.5 second as transition only lasts 0.5 second
+    transitionClearanceRef.current = setTimeout(() => {
+      if (windowRef.current.base) {
+        windowRef.current.base.style.transition = '';
+      }
+      transitionClearanceRef.current = 0;
+    }, 500);
+
     // When it's already maximized, revert the window to the previous size
-    if (windowSize.width === deskTopWidth && windowSize.height === desktopHeight) {
-      setWindowSize(originalSize);
-      setWindowPosition(originalPosition);
+    if (windowWidth === deskTopWidth && windowHeight === desktopHeight) {
+      windowRef.current.updateSize(originalSizeRef.current);
+      windowRef.current.updatePosition(originalPositionRef.current);
     }
     // Maximize the window to the size of the desktop
     else {
-      setOriginalSize(windowSize);
-      setOriginalPosition(windowPosition);
-      setWindowSize({
+      originalSizeRef.current = { width: windowWidth, height: windowHeight };
+      originalPositionRef.current = { x: windowLeft, y: windowTop };
+
+      windowRef.current.updateSize({
         height: desktopHeight,
         width: deskTopWidth,
       });
-      setWindowPosition({
+
+      windowRef.current.updatePosition({
         x: document.body.clientWidth / 2,
         y: 0,
       });
@@ -86,10 +135,16 @@ export const Window = ({ appID }: WindowProps) => {
 
   return (
     <Rnd
-      className={css.window}
+      ref={(c) => {
+        if (c) windowRef.current = c;
+      }}
       style={{ zIndex: appZIndex }}
-      size={windowSize}
-      position={windowPosition}
+      default={{
+        height: 500,
+        width: 600,
+        x: ((3 / 2) * document.body.clientWidth + randX) / 2,
+        y: (100 + randY) / 2,
+      }}
       enableResizing={resizable}
       dragHandleClassName="app-window-drag-handle"
       bounds="parent"
