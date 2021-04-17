59142fa2413cf437ce954aa3b370fbd38c060883
aslemammad <zorofight94@gmail.com>
2021-04-17 18:20:40 +0430

    remove tippy.js

--- src/components/topbar/menubar/MenuBar.tsx
+++ src/components/topbar/menubar/MenuBar.tsx
@@ -1,12 +1,13 @@
-import Tippy from '@tippyjs/react/headless';
 import clsx from 'clsx';
 import { useAtom } from 'jotai';
 import { useRef, useState } from 'preact/hooks';
+import { mdiApple } from '@mdi/js';
 import { ButtonBase } from '__/components/utils/ButtonBase';
 import { useOutsideClick } from '__/hooks';
 import { activeMenuStore, menuBarMenusStore } from '__/stores/menubar.store';
 import { Menu } from './Menu';
 import css from './MenuBar.module.scss';
+import { AppIcon } from '../../utils/AppIcon';
 
 const popperOptions = {
   modifiers: [
@@ -22,58 +23,59 @@ const popperOptions = {
 export const MenuBar = () => {
   const [currentAppMenus] = useAtom(menuBarMenusStore);
   const [activeMenu, setActiveMenu] = useAtom(activeMenuStore);
-  const [forceClosed, setForceCLosed] = useState(false);
+  const [forceClosed, setForceClosed] = useState(false);
 
   const parentRef = useRef<HTMLDivElement>();
 
   useOutsideClick(parentRef, () => {
     // If no menu open, then ignore
     // set force close here cuz clicking anywhere else makes the menu stay closed after clicking on it
-    if (activeMenu === '') return setForceCLosed(false);
+    if (activeMenu === '') return setForceClosed(false);
 
     setActiveMenu('');
 
     // To override the animation
-    setForceCLosed(true);
+    setForceClosed(true);
   });
 
   return (
     <div className={css.container} ref={parentRef}>
       {Object.keys(currentAppMenus).map((menuID) => (
-        <Tippy
-          key={menuID}
-          visible={activeMenu === menuID}
-          placement="bottom-start"
-          animation={true}
-          zIndex={99999999}
-          popperOptions={popperOptions}
-          onHide={() => setForceCLosed(false)}
-          interactive
-          appendTo={document.body}
-          render={(attrs) => (
-            <div {...attrs}>
-              <Menu
-                isHidden={activeMenu !== menuID}
-                forceHidden={forceClosed}
-                menu={currentAppMenus[menuID].menu}
-              />
-            </div>
-          )}
-        >
+        <div key={menuID}>
           <span style={{ height: '100%' }}>
             <ButtonBase
-              onClick={() => setActiveMenu(menuID)}
+              onClick={() => {
+                setActiveMenu(menuID);
+                setForceClosed(false);
+              }}
               onMouseOver={() => activeMenu && setActiveMenu(menuID)}
               className={clsx({
                 [css.menuButton]: true,
                 [css.defaultMenu]: menuID === 'default',
+                [css.appleIconButton]: menuID === 'apple',
               })}
               style={{ '--scale': activeMenu === menuID ? 1 : 0 } as React.CSSProperties}
             >
-              {currentAppMenus[menuID].title}
+              {menuID === 'apple' ? (
+                <AppIcon size={18} path={mdiApple} />
+              ) : (
+                currentAppMenus[menuID].title
+              )}
             </ButtonBase>
           </span>
-        </Tippy>
+          <div
+            className={clsx(css.menuParent)}
+            style={{
+              visibility: activeMenu !== menuID ? 'hidden' : 'visible',
+            }}
+          >
+            <Menu
+              isHidden={activeMenu !== menuID}
+              forceHidden={forceClosed}
+              menu={currentAppMenus[menuID].menu}
+            />
+          </div>
+        </div>
       ))}
     </div>
   );
