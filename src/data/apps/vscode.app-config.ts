cd087312d3784bc42f51db1c4890910e3cca6346
Puru Vijay <devpuruvj@gmail.com>
2021-04-20 02:56:35 +0530

    Change default height and width

--- src/data/apps/vscode.app-config.ts
+++ src/data/apps/vscode.app-config.ts
@@ -3,4 +3,7 @@ import { createAppConfig } from '__/helpers/create-app-config';
 export const vscodeAppConfig = createAppConfig({
   title: 'VSCode',
   resizable: true,
+
+  height: 600,
+  width: 800,
 });
