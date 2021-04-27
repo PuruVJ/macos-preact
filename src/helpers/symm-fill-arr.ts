77d5b1f7577f74ad22aea2ad8f7b5cb4089d57f5
Puru Vijay <devpuruvj@gmail.com>
2021-04-27 21:09:07 +0530

    Ambien types for Array.fill

--- src/helpers/symm-fill-arr.ts
+++ src/helpers/symm-fill-arr.ts
@@ -2,16 +2,14 @@ export function symmFillArr(length: number, value: number, index: number) {
   let start = index;
   let end = index;
 
-  const arr = Array(length).fill(0).map(Number);
+  const arr = Array(length).fill(0);
 
   while (value >= 0) {
     if (start >= 0) arr[start] = value;
     if (end < length) arr[end] = value;
 
     value--;
-
-    start--;
-    end++;
+    start--, end++;
   }
 
   return arr;
