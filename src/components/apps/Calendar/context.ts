33ca9568bf0a0ee3cbfd2e0aa8aee673d0feb5c2
Puru Vijay <devpuruvj@gmail.com>
2021-04-28 02:45:01 +0530

    Merge

--- src/components/apps/Calendar/context.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'preact';
-import { ICalendarAppContext } from './types';
-
-export const CalendarAppContext = createContext<ICalendarAppContext>({} as ICalendarAppContext);
--- src/components/apps/Calendar/types.ts
+++
@@ -1,3 +0,0 @@
-export interface ICalendarAppContext {
-  selectedDate: Date;
-}
