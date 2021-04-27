6a87e401da4fe5d9c06e35648ccd93b8239a7880
Puru Vijay <devpuruvj@gmail.com>
2021-04-27 20:10:34 +0530

    JSX.IntrinsicElements on ButtonBase

--- src/components/utils/ButtonBase.tsx
+++ src/components/utils/ButtonBase.tsx
@@ -2,15 +2,14 @@ import clsx from 'clsx';
 import { forwardRef } from 'preact/compat';
 import css from './ButtonBase.module.scss';
 
-export const ButtonBase = forwardRef<
-  HTMLButtonElement,
-  React.HTMLAttributes<HTMLButtonElement> & { disabled?: boolean }
->(({ children, disabled = false, ...props }, ref) => {
-  const { class: className, ...rest } = props;
+export const ButtonBase = forwardRef<HTMLButtonElement, JSX.IntrinsicElements['button']>(
+  ({ children, disabled = false, ...props }, ref) => {
+    const { class: className, ...rest } = props;
 
-  return (
-    <button disabled={disabled} ref={ref} class={clsx(className, css.button)} {...rest}>
-      {children}
-    </button>
-  );
-});
+    return (
+      <button disabled={disabled} ref={ref} class={clsx(className, css.button)} {...rest}>
+        {children}
+      </button>
+    );
+  },
+);
