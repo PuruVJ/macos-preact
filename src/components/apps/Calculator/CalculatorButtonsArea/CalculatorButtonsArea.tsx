912766367724b5dee4e168b579c670f0cb488271
Nir Tamir <nirtamir2@gmail.com>
2021-05-28 13:12:39 +0300

    wip

--- src/components/apps/Calculator/CalculatorButtonsArea/CalculatorButtonsArea.tsx
+++ src/components/apps/Calculator/CalculatorButtonsArea/CalculatorButtonsArea.tsx
@@ -12,10 +12,10 @@ enum Operator {
 }
 
 enum Mode {
-  InitFirstNumber = 'InitFirstNumber',
-  Operator = 'Operator',
-  InitSecondNumber = 'InitSecondNumber',
-  ShowResult = 'ShowResult',
+  InsertFirstNumber = 'InsertFirstNumber',
+  InsertOperator = 'InsertOperator',
+  InsertSecondNumber = 'InsertSecondNumber',
+  ShowingResult = 'ShowingResult',
   Idle = 'Idle',
 }
 
@@ -45,72 +45,79 @@ export const CalculatorButtonsArea = () => {
   const [firstNumberText, setFirstNumberText] = useState('');
   const [secondNumberText, setSecondNumberText] = useState('');
   const [mode, setMode] = useState(Mode.Idle);
+  const [result, setResult] = useState('0');
 
-  function renderResult() {
-    switch (mode) {
-      case Mode.Idle:
-        return 0;
-      case Mode.InitFirstNumber:
-        return firstNumberText;
-      case Mode.InitSecondNumber:
-        return secondNumberText;
-      case Mode.Operator:
-        return secondNumberText.length
-          ? secondNumberText
-          : firstNumberText.length
-          ? firstNumberText
-          : 0;
-      case Mode.ShowResult: {
-        if (operator && firstNumberText && secondNumberText) {
-          const result = getMathResult({
-            first: Number(firstNumberText),
-            operator,
-            second: Number(secondNumberText),
-          });
-          return isNaN(result) ? 'Not a number' : result;
-        }
-        return firstNumberText;
-      }
-    }
-  }
+  // function renderResult() {
+  //   switch (mode) {
+  //     case Mode.Idle:
+  //       return 0;
+  //     case Mode.InsertFirstNumber:
+  //       return firstNumberText;
+  //     case Mode.InsertSecondNumber:
+  //       return secondNumberText;
+  //     case Mode.InsertOperator:
+  //       return secondNumberText.length
+  //         ? secondNumberText
+  //         : firstNumberText.length
+  //         ? firstNumberText
+  //         : 0;
+  //     case Mode.ShowingResult: {
+  //       if (operator && firstNumberText && secondNumberText) {
+  //         const result = getMathResult({
+  //           first: Number(firstNumberText),
+  //           operator,
+  //           second: Number(secondNumberText),
+  //         });
+  //         return isNaN(result) ? 'Not a number' : result;
+  //       }
+  //       return firstNumberText;
+  //     }
+  //   }
+  // }
 
   function handleReset() {
     setOperator(null);
     setFirstNumberText('');
     setSecondNumberText('');
     setMode(Mode.Idle);
+    setResult('0');
   }
 
   function handleChangeOperator(operatorValue: Operator) {
-    if (secondNumberText && operator && firstNumberText) {
-      setFirstNumberText(
-        `${getMathResult({
-          first: Number(firstNumberText),
-          operator,
-          second: Number(secondNumberText),
-        })}`,
-      );
-      setSecondNumberText('');
-    }
-    if (!firstNumberText) {
-      setFirstNumberText('0');
-    }
     setOperator(operatorValue);
-    setMode(Mode.Operator);
+    setMode(Mode.InsertOperator);
   }
 
   function handlePressDot() {
     switch (mode) {
       case Mode.Idle: {
-        setFirstNumberText(() => '0.1');
+        const result = '0.1';
+        setMode(Mode.InsertFirstNumber);
+        setFirstNumberText(result);
+        setResult(result);
         break;
       }
-      case Mode.InitSecondNumber: {
-        setSecondNumberText((num) => (num.includes('.') ? num : `${num}.`));
+      case Mode.InsertSecondNumber: {
+        const updatedResult = secondNumberText.includes('.')
+          ? secondNumberText
+          : `${secondNumberText}.`;
+        setSecondNumberText(updatedResult);
+        setResult(updatedResult);
         break;
       }
-      case Mode.InitFirstNumber: {
-        setFirstNumberText((num) => (num.includes('.') ? num : `${num}.`));
+      case Mode.InsertFirstNumber: {
+        const result = firstNumberText.includes('.') ? firstNumberText : `${firstNumberText}.`;
+        setFirstNumberText(result);
+        setResult(result);
+        break;
+      }
+      case Mode.InsertOperator:
+      case Mode.ShowingResult: {
+        setMode(Mode.InsertSecondNumber);
+        setFirstNumberText(result);
+        setSecondNumberText('0.1');
+        setOperator(null);
+        setResult('0.1');
         break;
       }
     }
@@ -118,68 +125,149 @@ export const CalculatorButtonsArea = () => {
 
   function handlePlusMinusOperator() {
     switch (mode) {
-      case Mode.InitFirstNumber: {
-        setFirstNumberText((num) => `${-Number(num)}`);
+      case Mode.InsertFirstNumber: {
+        const result = `${-Number(firstNumberText)}`;
+        setFirstNumberText(result);
+        setResult(result);
         break;
       }
-      case Mode.InitSecondNumber: {
-        setSecondNumberText((num) => `${-Number(num)}`);
+      case Mode.InsertSecondNumber: {
+        const result = `${-Number(secondNumberText)}`;
+        setSecondNumberText(result);
+        setResult(result);
         break;
       }
+      case Mode.ShowingResult: {
+        const updatedResult = `${-Number(result)}`;
+        setFirstNumberText(updatedResult);
+        setSecondNumberText('');
+        setOperator(null);
+        setResult(updatedResult);
+        break;
+      }
+      case Mode.InsertOperator: {
+        const updatedResult = `${-Number(result)}`;
+        if (secondNumberText) {
+          setSecondNumberText(updatedResult);
+        } else {
+          setFirstNumberText(updatedResult);
+        }
+        break;
+      }
+      case Mode.Idle:
+        break;
     }
   }
   function handlePercentOperator() {
     switch (mode) {
-      case Mode.InitFirstNumber: {
-        setFirstNumberText((num) => `${Number(num) / 100}`);
+      case Mode.InsertFirstNumber: {
+        const updatedResult = `${Number(result) / 100}`;
+        setFirstNumberText(updatedResult);
+        setResult(updatedResult);
         break;
       }
-      case Mode.InitSecondNumber: {
-        setSecondNumberText((num) => `${Number(num) / 100}`);
+      case Mode.InsertSecondNumber: {
+        const updatedResult = `${Number(result) / 100}`;
+        setSecondNumberText(updatedResult);
+        setResult(updatedResult);
+        break;
+      }
+      case Mode.InsertOperator: {
+        const updatedResult = `${Number(result) / 100}`;
+        if (secondNumberText) {
+          setSecondNumberText(updatedResult);
+        } else {
+          setFirstNumberText(updatedResult);
+        }
+        setResult(updatedResult);
+        break;
+      }
+      case Mode.ShowingResult: {
+        const updatedResult = `${Number(result) / 100}`;
+        if (secondNumberText) {
+          setSecondNumberText(updatedResult);
+        } else {
+          setFirstNumberText(updatedResult);
+        }
+        setResult(updatedResult);
         break;
       }
     }
   }
 
+  // After percent, when inerting first number the result appear but we can;t feed the number
   function handlePressNumber(number: number) {
     switch (mode) {
-      case Mode.Idle:
-      case Mode.InitFirstNumber: {
-        setFirstNumberText((existNumber) => `${existNumber}${number}`);
-        setMode(Mode.InitFirstNumber);
+      case Mode.Idle: {
+        const result = `${firstNumberText}${number}`;
+        setFirstNumberText(result);
+        setResult(result);
+        setMode(Mode.InsertFirstNumber);
         break;
       }
-      case Mode.InitSecondNumber: {
-        setSecondNumberText((existNumber) => `${existNumber}${number}`);
+      case Mode.InsertFirstNumber: {
+        const result = `${firstNumberText}${number}`;
+        setFirstNumberText(result);
+        setResult(result);
         break;
       }
-      case Mode.Operator: {
-        setMode(Mode.InitSecondNumber);
-        setSecondNumberText(`${number}`);
+      case Mode.InsertSecondNumber: {
+        const result = `${secondNumberText}${number}`;
+        setSecondNumberText(result);
+        setResult(result);
         break;
       }
-      case Mode.ShowResult: {
+      case Mode.InsertOperator: {
+        const result = `${number}`;
+        setSecondNumberText(result);
+        setResult(result);
+        setMode(Mode.InsertSecondNumber);
+        break;
+      }
+      case Mode.ShowingResult: {
         setOperator(null);
-        setFirstNumberText(`${number}`);
+        const result = `${number}`;
+        setFirstNumberText(result);
+        setResult(result);
         setSecondNumberText('');
-        setMode(Mode.InitFirstNumber);
+        setMode(Mode.InsertFirstNumber);
       }
     }
   }
 
   function handleShowResult() {
-    setMode(Mode.ShowResult);
+    if (firstNumberText && secondNumberText && operator) {
+      setFirstNumberText(result);
+      const updatedResult = `${getMathResult({
+        first: Number(firstNumberText),
+        operator,
+        second: Number(secondNumberText),
+      })}`;
+      setSecondNumberText(updatedResult);
+      setResult(updatedResult);
+    }
+    if (!firstNumberText) {
+      const result = '0';
+      setFirstNumberText(result);
+      setResult(result);
+    }
+    if (!secondNumberText || !operator) {
+      const result = `${Number(firstNumberText)}`;
+      setResult(result);
+      setFirstNumberText(result);
+    }
+    setMode(Mode.ShowingResult);
   }
 
   return (
     <>
-      <section class={css.showArea}>{renderResult()}</section>
+      <section class={css.showArea}>{result}</section>
       <section class={css.container}>
         <button class={css.topRowButton} onClick={handleReset}>
           AC
         </button>
-        <button class={css.topRowButton}>
-          <AppIcon path={mdiPlusMinusVariant} onClick={handlePlusMinusOperator} />
+        <button class={css.topRowButton} onClick={handlePlusMinusOperator}>
+          <AppIcon path={mdiPlusMinusVariant} />
         </button>
         <button class={css.topRowButton} onClick={handlePercentOperator}>
           <AppIcon path={mdiPercentOutline} />
