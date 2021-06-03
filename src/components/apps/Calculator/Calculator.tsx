9e9b76195209eb08b23e2d1cf9e2f0042ab30251
Nir Tamir <nirtamir2@gmail.com>
2021-06-03 23:47:52 +0300

    add reducer

--- src/components/apps/Calculator/Calculator.tsx
+++ src/components/apps/Calculator/Calculator.tsx
@@ -1,12 +1,181 @@
+import { mdiClose, mdiDivision, mdiMinus, mdiPercentOutline, mdiPlusMinusVariant } from '@mdi/js';
 import clsx from 'clsx';
-import { CalculatorButtonsArea } from './CalculatorButtonsArea/CalculatorButtonsArea';
+import { AppIcon } from '__/components/utils/AppIcon';
 import css from './Calculator.module.scss';
+import { useReducer } from 'react';
+import { calculatorReducer } from './calculatorReducer';
+
+export enum Operator {
+  Plus = 'Plus',
+  Minus = 'Minus',
+  Multiply = 'Multiply',
+  Divide = 'Divide',
+}
+
+export enum Mode {
+  InsertFirstNumber = 'InsertFirstNumber',
+  InsertOperator = 'InsertOperator',
+  InsertSecondNumber = 'InsertSecondNumber',
+  ShowingResult = 'ShowingResult',
+  Idle = 'Idle',
+}
+
+export interface IState {
+  operator: Operator | null;
+  firstNumberText: string;
+  secondNumberText: string;
+  mode: Mode;
+  result: string;
+}
+
+export const initialState: IState = {
+  operator: null,
+  firstNumberText: '',
+  secondNumberText: '',
+  mode: Mode.Idle,
+  result: '0',
+};
+
+export type ActionT =
+  | {
+      type: 'Reset';
+    }
+  | {
+      type: 'ChangeOperator';
+      payload: {
+        operatorValue: Operator;
+      };
+    }
+  | {
+      type: 'PressDot';
+    }
+  | {
+      type: 'PlusMinusOperator';
+    }
+  | {
+      type: 'PercentOperator';
+    }
+  | {
+      type: 'PressNumber';
+      payload: {
+        number: number;
+      };
+    }
+  | {
+      type: 'ShowResult';
+    };
 
 const Calculator = () => {
+  const [state, dispatch] = useReducer<React.Reducer<IState, ActionT>>(
+    calculatorReducer,
+    initialState,
+  );
+
+  const { result } = state;
+
+  function handleReset() {
+    dispatch({ type: 'Reset' });
+  }
+
+  function handleChangeOperator(operatorValue: Operator) {
+    dispatch({ type: 'ChangeOperator', payload: { operatorValue } });
+  }
+
+  function handlePressDot() {
+    dispatch({ type: 'PressDot' });
+  }
+
+  function handlePlusMinusOperator() {
+    dispatch({ type: 'PlusMinusOperator' });
+  }
+
+  function handlePercentOperator() {
+    dispatch({ type: 'PercentOperator' });
+  }
+
+  // After percent, when inerting first number the result appear but we can;t feed the number
+  function handlePressNumber(number: number) {
+    dispatch({ type: 'PressNumber', payload: { number } });
+  }
+
+  function handleShowResult() {
+    dispatch({ type: 'ShowResult' });
+  }
+
   return (
     <section class={css.container}>
-      <header class={clsx('app-window-drag-handle', css.header)}></header>
-      <CalculatorButtonsArea />
+      <header class={clsx('app-window-drag-handle', css.header)}/>
+      <section class={css.showArea}>{result}</section>
+      <section class={css.buttonsContainer}>
+        <button class={css.topRowButton} onClick={handleReset}>
+          AC
+        </button>
+        <button class={css.topRowButton} onClick={handlePlusMinusOperator}>
+          <AppIcon path={mdiPlusMinusVariant} />
+        </button>
+        <button class={css.topRowButton} onClick={handlePercentOperator}>
+          <AppIcon path={mdiPercentOutline} />
+        </button>
+        <button class={css.operationButton} onClick={() => handleChangeOperator(Operator.Divide)}>
+          <AppIcon path={mdiDivision} />
+        </button>
+
+        <button class={css.numberButton} onClick={() => handlePressNumber(7)}>
+          7
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressNumber(8)}>
+          8
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressNumber(9)}>
+          9
+        </button>
+        <button class={css.operationButton} onClick={() => handleChangeOperator(Operator.Multiply)}>
+          <AppIcon path={mdiClose} />
+        </button>
+
+        <button class={css.numberButton} onClick={() => handlePressNumber(4)}>
+          4
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressNumber(5)}>
+          5
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressNumber(6)}>
+          6
+        </button>
+        <button class={css.operationButton} onClick={() => handleChangeOperator(Operator.Minus)}>
+          <AppIcon path={mdiMinus} size={24} />
+        </button>
+
+        <button class={css.numberButton} onClick={() => handlePressNumber(1)}>
+          1
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressNumber(2)}>
+          2
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressNumber(3)}>
+          3
+        </button>
+        <button class={css.operationButton} onClick={() => handleChangeOperator(Operator.Plus)}>
+          +
+        </button>
+
+        <button
+          class={clsx(css.numberButton, css.curvedBottomLeftButton)}
+          style={{ gridColumn: '1 / span 2' }}
+          onClick={() => handlePressNumber(0)}
+        >
+          0
+        </button>
+        <button class={css.numberButton} onClick={() => handlePressDot()}>
+          .
+        </button>
+        <button
+          class={clsx(css.operationButton, css.curvedBottomRightButton)}
+          onClick={handleShowResult}
+        >
+          =
+        </button>
+      </section>
     </section>
   );
 };
---
+++ src/components/apps/Calculator/calculatorReducer.tsx
@@ -0,0 +1,235 @@
+import {ActionT, initialState, IState, Mode, Operator} from "./Calculator";
+
+function getMathResult({
+  first,
+  operator,
+  second,
+}: {
+  first: number;
+  operator: Operator;
+  second: number;
+}): number {
+  switch (operator) {
+    case Operator.Plus:
+      return first + second;
+    case Operator.Minus:
+      return first - second;
+    case Operator.Multiply:
+      return first * second;
+    case Operator.Divide:
+      return first / second;
+  }
+}
+
+export function calculatorReducer(state: typeof initialState, action: ActionT): IState {
+  switch (action.type) {
+    case 'Reset':
+      return initialState;
+    case 'ChangeOperator': {
+      const { operatorValue } = action.payload;
+      return { ...state, operator: operatorValue, mode: Mode.InsertOperator };
+    }
+    case 'PressDot': {
+      switch (state.mode) {
+        case Mode.Idle: {
+          const result = '0.1';
+          return {
+            ...state,
+            mode: Mode.InsertFirstNumber,
+            firstNumberText: result,
+            result: result,
+          };
+        }
+        case Mode.InsertSecondNumber: {
+          const { secondNumberText } = state;
+          const updatedResult = secondNumberText.includes('.')
+            ? secondNumberText
+            : `${secondNumberText}.`;
+          return {
+            ...state,
+            result: updatedResult,
+            secondNumberText: updatedResult,
+          };
+        }
+        case Mode.InsertFirstNumber: {
+          const { firstNumberText } = state;
+          const result = firstNumberText.includes('.') ? firstNumberText : `${firstNumberText}.`;
+          return { ...state, firstNumberText: result, result: result };
+        }
+        case Mode.InsertOperator:
+        case Mode.ShowingResult: {
+          return {
+            mode: Mode.InsertSecondNumber,
+            firstNumberText: state.result,
+            secondNumberText: '0.1',
+            operator: null,
+            result: '0.1',
+          };
+        }
+      }
+      return state;
+    }
+    case 'PlusMinusOperator': {
+      switch (state.mode) {
+        case Mode.InsertFirstNumber: {
+          const result = `${-Number(state.firstNumberText)}`;
+          return { ...state, firstNumberText: result, result };
+        }
+        case Mode.InsertSecondNumber: {
+          const result = `${-Number(state.secondNumberText)}`;
+          return { ...state, secondNumberText: result, result };
+        }
+        case Mode.ShowingResult: {
+          const updatedResult = `${-Number(state.result)}`;
+          return {
+            ...state,
+            firstNumberText: updatedResult,
+            secondNumberText: '',
+            operator: null,
+            result: updatedResult,
+          };
+        }
+        case Mode.InsertOperator: {
+          const updatedResult = `${-Number(state.result)}`;
+          if (state.secondNumberText) {
+            return {
+              ...state,
+              secondNumberText: updatedResult,
+            };
+          } else {
+            return {
+              ...state,
+              firstNumberText: updatedResult,
+            };
+          }
+        }
+        case Mode.Idle:
+          return state;
+      }
+      return state;
+    }
+    case 'PercentOperator': {
+      const { result } = state;
+
+      switch (state.mode) {
+        case Mode.InsertFirstNumber: {
+          const updatedResult = `${Number(result) / 100}`;
+          return {
+            ...state,
+            firstNumberText: updatedResult,
+            result: updatedResult,
+          };
+        }
+        case Mode.InsertSecondNumber: {
+          const updatedResult = `${Number(result) / 100}`;
+          return {
+            ...state,
+            secondNumberText: updatedResult,
+            result: updatedResult,
+          };
+        }
+        case Mode.InsertOperator:
+        case Mode.ShowingResult: {
+          const updatedResult = `${Number(result) / 100}`;
+          if (state.secondNumberText) {
+            return {
+              ...state,
+              secondNumberText: updatedResult,
+              result: updatedResult,
+            };
+          } else {
+            return {
+              ...state,
+              firstNumberText: updatedResult,
+              result: updatedResult,
+            };
+          }
+        }
+      }
+      return state;
+    }
+    case 'PressNumber': {
+      const { number } = action.payload;
+      switch (state.mode) {
+        case Mode.Idle: {
+          const result = `${state.firstNumberText}${number}`;
+          return {
+            ...state,
+            firstNumberText: result,
+            result: result,
+            mode: Mode.InsertFirstNumber,
+          };
+        }
+        case Mode.InsertFirstNumber: {
+          const result = `${state.firstNumberText}${number}`;
+          return { ...state, firstNumberText: result, result: result };
+        }
+        case Mode.InsertSecondNumber: {
+          const result = `${state.secondNumberText}${number}`;
+          return { ...state, secondNumberText: result, result: result };
+        }
+        case Mode.InsertOperator: {
+          const result = `${number}`;
+          return {
+            ...state,
+            secondNumberText: result,
+            result: result,
+            mode: Mode.InsertSecondNumber,
+          };
+        }
+        case Mode.ShowingResult: {
+          const result = `${number}`;
+          return {
+            operator: null,
+            firstNumberText: result,
+            result: result,
+            secondNumberText: '',
+            mode: Mode.InsertFirstNumber,
+          };
+        }
+      }
+      return state;
+    }
+    case 'ShowResult': {
+      {
+        const { firstNumberText, secondNumberText, operator } = state;
+        if (firstNumberText && secondNumberText && operator) {
+          const updatedResult = `${getMathResult({
+            first: Number(firstNumberText),
+            operator,
+            second: Number(secondNumberText),
+          })}`;
+          return {
+            ...state,
+            mode: Mode.ShowingResult,
+            firstNumberText: state.result,
+            secondNumberText: updatedResult,
+            result: updatedResult,
+          };
+        }
+        if (!firstNumberText) {
+          return {
+            ...state,
+            mode: Mode.ShowingResult,
+            firstNumberText: '0',
+            result: '0',
+          };
+        }
+        if (!secondNumberText || !operator) {
+          const result = `${Number(firstNumberText)}`;
+          return {
+            ...state,
+            mode: Mode.ShowingResult,
+            result: result,
+            firstNumberText: result,
+          };
+        }
+      }
+
+      return { ...state, mode: Mode.ShowingResult };
+    }
+    default:
+      throw new Error();
+  }
+}
+
