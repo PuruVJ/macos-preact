43edfaed87a884d03f9ac48ffe5c0b5e6f7f8701
Jose Sanchez Aranda <jose.sanchez@shapelets.io>
2021-06-03 16:25:17 +0200

    set property autoplay instead of use refs

--- src/components/Desktop/StartupChime.tsx
+++ src/components/Desktop/StartupChime.tsx
@@ -1,21 +1,12 @@
 import { mdiApple } from '@mdi/js';
 import clsx from 'clsx';
-import { useEffect, useRef, useState } from 'preact/hooks';
+import { useState } from 'preact/hooks';
 import { useTimeout } from '__/hooks';
 import { AppIcon } from '../utils/AppIcon';
 import css from './StartupChime.module.scss';
 
 export const StartupChime = () => {
-  const ref = useRef<HTMLButtonElement>();
-  const audioRef = useRef<HTMLAudioElement>();
-
   const [hiddenSplashScreen, setHiddenSplashScreen] = useState(false);
-
-  useEffect(() => {
-    ref.current?.click();
-    // Disable playing again and again in dev environment
-    if (import.meta.env.PROD) audioRef.current.play();
-  }, []);
 
   useTimeout(() => {
     setHiddenSplashScreen(true);
@@ -26,16 +17,13 @@ export const StartupChime = () => {
       <div
         class={clsx({
           [css.splashScreen]: true,
-          [css.hidden]: hiddenSplashScreen || import.meta.env.DEV,
+          [css.hidden]: hiddenSplashScreen || import.meta.env.DEV
         })}
         hidden={hiddenSplashScreen}
       >
         <AppIcon path={mdiApple} fill="white" size={100} />
       </div>
-
-      <button hidden ref={ref}></button>
-
-      <audio hidden src="/assets/sounds/mac-startup-sound.mp3" ref={audioRef} />
+      <audio hidden autoPlay={import.meta.env.PROD} src="/assets/sounds/mac-startup-sound.mp3" />
     </>
   );
 };
