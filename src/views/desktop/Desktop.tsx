f8d5571d835636cdb1afeb69506d1ef9ad49b9b0
aslemammad <zorofight94@gmail.com>
2021-06-16 14:53:35 +0430

    draft fix

--- src/views/desktop/Desktop.tsx
+++ src/views/desktop/Desktop.tsx
@@ -12,6 +12,8 @@ export const Desktop = () => {
   const outerRef = useRef<HTMLDivElement>();
 
   const [wallpaperName, setWallpaperName] = useWallpaperName();
+  setWallpaperName('monterey');
+  console.log('hello', wallpaperName);
   const [currWallpaperImg] = useTimelyWallpapers();
 
   const [wallpaper, setWallpaper] = useState('');
